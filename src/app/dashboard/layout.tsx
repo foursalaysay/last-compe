a18ee4c03340d141a3f69d18bccec8e7e6eac2b1
Michael G. Padin <padinmichael201@gmail.com>
2024-04-24 03:00:32 +0800

    update dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
 
       <div className="w-full lg:ml-72 ">
         <Navbar />
-        <ScrollArea className="m-auto h-[calc(100vh-72.8px)] bg-muted dark:bg-background">
+        <ScrollArea className="m-auto h-[calc(100vh-72.8px)]">
           {children}
         </ScrollArea>
       </div>
