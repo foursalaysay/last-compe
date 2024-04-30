6e7f0aabde9b18162e6f8d749b1f031d723f75c8
Michael G. Padin <padinmichael201@gmail.com>
2024-04-30 21:25:51 +0800

    refactor dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,6 +4,7 @@ import { ScrollArea } from "@/components/ui/scroll-area"
 
 import Navbar from "./_components/navbar"
 import Profile from "./_components/profile"
+import Sidebar from "./_components/sidebar"
 import SidebarMenuItems from "./_components/sidebar-menu-items"
 
 export default function RootLayout({
@@ -12,33 +13,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex w-full overflow-hidden">
-      <div
-        className={`fixed bottom-0 left-0 top-0 z-50 hidden w-72   flex-col border-r  bg-background transition-transform  duration-300 md:translate-x-0 lg:flex`}
-      >
-        <div className="mx-4 flex items-center justify-between md:justify-center">
-          <a href="#" className="flex h-20  items-center"></a>
-          <h1 className="text-2xl font-black">
-            <Link href={"/"}>
-              <span className="text-primary">Share</span>Surplus
-            </Link>
-          </h1>
-          {/* <X
-						onClick={closeSidebar}
-						className="block cursor-pointer md:hidden"
-					/> */}
-        </div>
-        <ScrollArea>
-          <SidebarMenuItems />
-        </ScrollArea>
-        <Profile />
-      </div>
-
-      <div className="w-full lg:ml-72 ">
+    <div className="flex overflow-hidden">
+      <Sidebar />
+      <div className="h-screen flex-1 overflow-hidden">
         <Navbar />
-        <ScrollArea className="m-auto h-[calc(100vh-72.8px)]">
-          {children}
-        </ScrollArea>
+        <main className="w-full">{children}</main>
       </div>
     </div>
   )
