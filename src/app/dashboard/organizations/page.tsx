688a2c8287bac114086a7542531009024f4bcf29
Michael G. Padin <padinmichael201@gmail.com>
2024-04-30 21:26:12 +0800

    add breadcrumb to organizations page

--- src/app/dashboard/organizations/page.tsx
+++ src/app/dashboard/organizations/page.tsx
@@ -5,11 +5,17 @@ import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
+import BreadCrumb from "../_components/breadcrumb"
 import OrganizationList from "./_components/organization-list"
+
+const breadcrumbItems = [
+  { title: "Organizations", link: "/dashboard/organizations" },
+]
 
 const OrganizationPage = () => {
   return (
-    <div className=" space-y-6 p-5 pb-16 ">
+    <div className="flex-1 space-y-4  p-4 pt-6 md:p-8">
+      <BreadCrumb items={breadcrumbItems} />
       <div className="flex w-full justify-between space-y-0.5">
         <div className="">
           <h2 className="flex items-center gap-2 text-2xl font-bold tracking-tight">
